a3b7f90ba2b48bd69c218c8acca35b1b53855e9f
DevSouza-Silas <72321332+DevSouza-Silas@users.noreply.github.com>
2020-10-10 20:57:30 -0300

    Alterei o nome dos pacotes do projeto pra ficar no padrão.

--- Pi3_Aluguel/src/main/java/br/fvc/pi3/model/Aluguel.java
+++ Pi3_Aluguel/src/main/java/br/fvc/pi3/model/Aluguel.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package modelo;
+package br.fvc.pi3.model;
 
 public class Aluguel {
     private Integer idaluguel;
